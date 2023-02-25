a90668e387e9896d563f8aba2954101893327fa9
Cezary Dynak <cezary.dynak@gmail.com>
2023-02-25 19:23:18 +0100

    import commit

---
+++ precache-manifest.eb19ad95bcee9bef8f86a35aac7ea9d4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c97b5601b27b4c85a5bb10099812c5cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26509c7b619b0ea26190",
+    "url": "/static/css/2.fa6c921b.chunk.css"
+  },
+  {
+    "revision": "bb88f06166a2d7f8851c",
+    "url": "/static/css/main.7c4273b2.chunk.css"
+  },
+  {
+    "revision": "26509c7b619b0ea26190",
+    "url": "/static/js/2.791655dd.chunk.js"
+  },
+  {
+    "revision": "ae2abe34a15973090e37b170cc414fbb",
+    "url": "/static/js/2.791655dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb88f06166a2d7f8851c",
+    "url": "/static/js/main.249475b2.chunk.js"
+  },
+  {
+    "revision": "c5c3170dc16abdc10e30",
+    "url": "/static/js/runtime-main.8ea680e9.js"
+  },
+  {
+    "revision": "a83f83f8b3db9c31605ca3f1a5699800",
+    "url": "/static/media/slider2X_750px.a83f83f8.png"
+  },
+  {
+    "revision": "3264c8718cd956f6009e80d48b7da3b6",
+    "url": "/static/media/tenby-five.3264c871.otf"
+  },
+  {
+    "revision": "b812192081ce06b5ea67eab218035d9f",
+    "url": "/static/media/worker.b8121920.png"
+  },
+  {
+    "revision": "e5a1c0959df27de188eb26fb8f6d55d0",
+    "url": "/static/media/worker.e5a1c095.png"
+  }
+]);
